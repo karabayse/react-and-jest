d5eac13613efbe0c127ee3c4714b2493019a8662
Kara Bayse <karabayse@gmail.com>
2018-10-11 16:09:35 -0500

    added handleSubmit test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+test('User name and city should be empty strings', () => {
+  expect(handleSubmit.setState()).toEqual({ user.name: '', user.city: '' });
 });
